43f3bbf5631788c74d689d340afbeb6a79d8ca08
GpamerXd <113861536+GpamerXd@users.noreply.github.com>
2023-03-11 09:56:13 +0300

    Add files via upload

---
+++ lab3.2.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+void input(int*& arr, int& size) {
+	if (arr != nullptr) {
+		delete[] arr;//возвращает выделенную память оператора new
+	}
+	std::cout << "Введите размер массива: ";
+	std::cin >> size;
+	arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		std::cout << "Введите число № " << i + 1 << "-> ";
+		std::cin >> arr[i];
+	}
+}
+void output(int* arr, int n) {
+	std::cout << "{ ";
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << "}";
+}
+int funcsum(int a1) {
+	int sum1 = 0;
+	while (a1 != 0) {
+		sum1 += (a1 % 100) % 10;
+		a1 /= 100;
+	}
+	return sum1;
+}
+int sort1(int* arr, int k) {
+	std::cout << std::endl;
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			if (funcsum(arr[j]) > funcsum(arr[j + 1])) {
+				int b = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = b;
+			}
+		}
+	}
+	std::cout << "Сортировка выполнена \n";
+	return *arr;
+}
+int sort2(int* arr, int k) {
+	std::cout << std::endl;
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			if (arr[j] % 10 > arr[j + 1] % 10) {
+				int b = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = b;
+			}
+		}
+	}
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			if (arr[j] < arr[j + 1] and arr[j] % 10 == arr[j + 1] % 10) {
+				int b = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = b;
+			}
+		}
+	}
+	return *arr;
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int choice, n;
+	int* arr = nullptr;
+	while (true) {
+		std::cout << "Что вы хотите выполнить? \n"
+			<< "1. Ввод массива\n"
+			<< "2. Вывод массива\n"
+			<< "3. Сортировка по сумме цифр, стоящих на чётных местах\n"
+			<< "4. Сортировка массива вначале по возрастанию последней цифры, а при сопадении последних цирф - по убыванию\n"
+			<< "5. Выход\n";
+		std::cin >> choice;
+		switch (choice) {// выбор задания
+		case 1: {
+			input(arr, n);
+			break;
+		}
+		case 2: {
+			output(arr, n);
+			break;
+		}
+		case 3: {
+			sort1(arr, n);
+			break;
+		}
+		case 4: {
+			sort2(arr, n);
+			break;
+		}
+		default: {//при выборе цифры вне () возвращает в начало выбора
+			delete[] arr;
+			return 0;
+		}
+		}
+	}
+}
---
+++ lab4.2.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+#include <cstring>
+#include <string>//заголовки стандартных библиотек
+#include <fstream>//работа с внешними файлами
+#include <bitset>//контейнер для хранения битов
+
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+
+string file_format(const string file_path_full) {
+	return file_path_full.substr(file_path_full.rfind("."), -1);
+}
+
+
+string file_name(const string file_path_full) {
+	return file_path_full.substr(file_path_full.rfind("\\"), file_path_full.rfind("."));
+}
+
+string file_path(const string file_path_full) {
+	return file_path_full.substr(0, file_path_full.rfind("\\"));
+}
+
+string file_disc(const string file_path_full) {
+	return file_path_full.substr(0, file_path_full.rfind(":"));
+}
+
+bool file_rename(string file_path_full) {
+	setlocale(LC_ALL, "Russian");
+	string f;
+	string new_name;
+	cin >> new_name;
+
+	f = file_path(file_path_full) + "\\" + new_name + "." + file_format(file_path_full);
+	cout << f;
+	return 1;
+
+}
+
+bool file_copy(string file_path_full) {
+	std::ifstream fin(file_path_full);//для работы с файлами
+	string copy = file_path(file_path_full) + '\\' + file_name(file_path_full) + "_copy" + "." + file_format(file_path_full);
+	std::ofstream fout(copy);
+	char temp;
+	if (!fin && !fout) {
+		cout << "Файлы не открыты";
+	}
+	else {
+		while (!fin.eof()) {
+			fin >> temp;//ввод
+			fout << temp;//вывод из файлов
+		}
+	}
+	fin.close();
+	fout.close();
+	return 1;
+}
+
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	string file_path_full = "C:\\Users\\Gamepro4sv\\Desktop\\папка\\for4.2.txt";
+
+	while (true) {
+		int m;
+		cin >> m;
+		switch (m) {
+		case 1: {
+			cout << file_format(file_path_full) << endl;
+			break;
+		}
+		case 2: {
+			cout << file_name(file_path_full) << endl;
+			break;
+		}
+		case 3: {
+			cout << file_path(file_path_full) << endl;
+			break;
+		}
+		case 4: {
+			cout << file_disc(file_path_full) << endl;
+			break;
+		}
+		case 5: {
+			file_rename(file_path_full);
+			break;
+		}
+		case 6: {
+			file_copy(file_path_full);
+		}
+		}
+	}
+}
